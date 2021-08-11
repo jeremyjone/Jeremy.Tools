1a355f754f8b38baa12df5dcb7f0b95f3c950fc7
jeremyjone <jeremyjone@qq.com>
2021-08-11 10:35:43 +0800

    添加仓库内容

---
+++ Repository/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Jeremy.Tools.Repository.Interfaces
+{
+    /// <summary>
+    /// Repository 的基类接口。如果不想使用基础功能，而是仅仅声明一个不包含任何内容的 Repository，使用它。
+    /// </summary>
+    public interface IRepository
+    {
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using Jeremy.Tools.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jeremy.Tools.Repository
+{
+    public class Repository<TContext, TRepository> : IRepository
+        where TContext : DbContext
+        where TRepository : IRepository
+    {
+        public TContext Db { get; }
+        protected ILogger<TRepository> Logger { get; }
+
+        public Repository(TContext db, ILogger<TRepository> logger)
+        {
+            Db = db;
+            Logger = logger;
+        }
+    }
+}
